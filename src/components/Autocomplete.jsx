9844370456b9eb7bc16dae33b80178265b563373
Sameer Saurabh <76647384+Sammy1836@users.noreply.github.com>
2024-06-23 12:22:08 +0530

    Update Autocomplete.jsx

--- src/components/Autocomplete.jsx
+++ src/components/Autocomplete.jsx
@@ -32,7 +32,7 @@ const Autocomplete = ({ placeholder, reducer }) => {
                     {
                         params: {
                             text: value,
-                            apiKey: import.meta.env.VITE_API_KEY,
+                            apiKey: API_KEY,
                         }
                     }
                 );
